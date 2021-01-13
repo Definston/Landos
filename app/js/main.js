9d24a777419e5f67c009451cfb3a5aaf5f86dada
Definston <definston@gmail.com>
2021-01-13 06:03:43 +0600

    added ds v.

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,18 @@
-$(function(){
-  
+$(function () {
+    $('.slider-items').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+    });
+
+    $(document).ready(function() {
+        $('img').magnificPopup({
+            type:'image'
+        });
+      });
+
+      $('.popup-button').magnificPopup({
+        type:'inline',
+        midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+      });
 
 });
